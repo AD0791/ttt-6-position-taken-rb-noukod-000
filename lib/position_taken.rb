bc9b8292a0661ccdf3c7ec02d55166f07338a4ec
Alexandro Disla <alexandrodisla@hotmail.com>
2019-04-05 19:54:36 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,9 @@
 def position_taken?(board,index)
   if board[index] == "X" && board[index] == "O"
     true
-  end
-  if board[index] == "" || board[index] == " " || board[index] == nil
+    if board[index] == "" || board[index] == " " || board[index] == nil
     false
+    end
   end
+  
 end
